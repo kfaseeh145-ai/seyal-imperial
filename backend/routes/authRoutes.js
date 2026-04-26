e481719e7d7248408c089c6c1c4dc5d9179703fd
kfaseeh145-ai <kfaseeh145@gmail.com>
2026-04-26 05:50:57 +0500

    Remove legacy backend folder

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, verifyUser, loginUser } = require('../controllers/authController');
-
-router.post('/register', registerUser);
-router.post('/verify', verifyUser);
-router.post('/login', loginUser);
-
-module.exports = router;
