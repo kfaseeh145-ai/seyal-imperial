52f91a0c19a5568d20e01f280e9a74b03860a98e
kfaseeh145-ai <kfaseeh145@gmail.com>
2026-04-26 05:40:14 +0500

    Final cleanup of backend

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET || 'fallback_secret_for_local_simulations', {
-    expiresIn: '30d',
-  });
-};
-
-module.exports = generateToken;
